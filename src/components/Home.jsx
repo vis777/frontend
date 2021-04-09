274ed32585db7223138911bbeaa641d3b72cd3fa
vis777 <82163417+vis777@users.noreply.github.com>
2021-04-09 14:09:58 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return <div className='container'>
+        <h1>Home page</h1>
+        <p>
+            A warm welcome message works like a charm in any situation, be it the recruitment of a new employee or having returning ones back, or meeting friends after a long time! Welcome messages are also appreciated in more formal settings like welcoming a guest or customer. Whatever the arrangement is, these notes convey your excitement and happiness towards the other parties and help them settle in more comfortably. So if you have a new party to meet and greet them cordially, pass along a sweet, meaningful welcome message while offering them a genuine smile! Perfect welcome messages can be hard to find, so help yourselves with a compilation of unique welcoming messages and notes below!
+            </p>
+    </div>
+}
+
+export default Home;
